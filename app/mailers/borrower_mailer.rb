d5ec9ed4dcdea3e47c90353609ddba75a77c86f2
Mike <mike@hotmail.com>
2015-06-02 14:24:55 -0700

    Email the borrower asynchronously

--- app/mailers/borrower_mailer.rb
+++ app/mailers/borrower_mailer.rb
@@ -2,6 +2,6 @@ class BorrowerMailer < ApplicationMailer
   default from: 'knuepwebdev@gmail.com'
 
   def order_notification(email_address)
-    mail to: email_address, subject: 'An order has been placed'
+    mail to: email_address, subject: 'Order Confirmation'
   end
 end
--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -1,11 +1,11 @@
 class EmailWorker
   include Sidekiq::Worker
 
-  def perform(user_id)
+  def perform(borrower_id)
     puts 'perform*'
-    borrower = Borrower.find(user_id)
-    puts 'borrower.email'
-    puts borrower.email
+    borrower = Borrower.find(borrower_id)
     BorrowerMailer.order_notification(borrower.email).deliver
+    # lender = Lender.find(lender_id)
+    # LenderMailer.order_notification(lender.email).deliver
   end
 end
